df6bd24245fa22d55b87474c4d3eced2697bdca0
misha-pelykh <jscript@meta.ua>
2017-03-17 19:36:11 +0200

    used path.join in server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 const port = process.env.PORT || 3000;
