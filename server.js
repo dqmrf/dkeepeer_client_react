a23ae29e4f16f0222fa717c44a3f836db346399d
misha-pelykh <jscript@meta.ua>
2017-03-17 19:57:33 +0200

    added body-parser & createServer

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || '4200';
+app.set('port', port);
 
-app.listen(port, () => {
-  console.log('app listening on', port);
-});
+const server = http.createServer(app);
+server.listen(port, () => console.log(`APP listening on port: ${port}`));
